7d43dfe783375497f637c983501d930a7268bd0d
r <admin@linkvehicle.com>
2018-09-28 11:53:40 -0400

    meta tags

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <meta content="Vietnamese food, Vietnamese resturant, stratford, ct" name="keywords" data-n-head="true" data-hid="keywords">
+
+        <meta content="TD Vietnam Palace Vietnamese dishes in a laid-back setting. Located in Stratford, CT serving traditional food made with fresh, high quality ingredients." name="description" data-n-head="true" data-hid="description">
+        <meta content="TD Vietnam Palace Vietnamese dishes in a laid-back setting. Located in Stratford, CT serving traditional food made with fresh, high quality ingredients." name="og:description" data-n-head="true" data-hid="og:description">
+
+        <meta content="/assets/images/pho.png" name="twitter:image" data-n-head="true" data-hid="twitter:image">
+        <meta content="/assets/images/pho.png" name="og:image" data-n-head="true" data-hid="og:image">
+
+        <meta content="TD Vietnam Palace | Vietnamese Resturant" name="og:title" data-n-head="true" data-hid="og:title">
+        <meta content="summary_large_image" name="twitter:card" data-n-head="true" data-hid="twitter:card">
+        <title data-n-head="true">TD Vietnam Palace | Vietnamese Resturant</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body>
