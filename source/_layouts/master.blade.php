11bba4e04521e8501fa45643b7d0222f0b6cc7bd
r <admin@linkvehicle.com>
2018-09-28 12:22:07 -0400

    new pic

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -10,8 +10,8 @@
         <meta content="TD Vietnam Palace Vietnamese dishes in a laid-back setting. Located in Stratford, CT serving traditional food made with fresh, high quality ingredients." name="description" data-n-head="true" data-hid="description">
         <meta content="TD Vietnam Palace Vietnamese dishes in a laid-back setting. Located in Stratford, CT serving traditional food made with fresh, high quality ingredients." name="og:description" data-n-head="true" data-hid="og:description">
 
-        <meta content="/assets/images/pho.jpg" name="twitter:image" data-n-head="true" data-hid="twitter:image">
-        <meta content="/assets/images/pho.jpg" name="og:image" data-n-head="true" data-hid="og:image">
+        <meta content="/assets/images/pho-bright.jpg" name="twitter:image" data-n-head="true" data-hid="twitter:image">
+        <meta content="/assets/images/pho-bright.jpg" name="og:image" data-n-head="true" data-hid="og:image">
 
         <meta content="TD Vietnam Palace | Vietnamese Resturant" name="og:title" data-n-head="true" data-hid="og:title">
         <meta content="summary_large_image" name="twitter:card" data-n-head="true" data-hid="twitter:card">
